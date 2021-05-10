47f71de7da7e1eca403ff3be20d51ec0066e8784
jameszhou123 <jameszhou@outlook.com>
2021-05-10 06:17:38 +0100

    add merged:

--- Data.py
+++ Data.py
@@ -1,20 +1,29 @@
 
 # In[ ]:
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+import seaborn as seabpd
+
+
+urlGlabalConfirmed = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+urlGlabalRecovered = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
+urlGlabalDeaths = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+
+countries = ['Date', 'France', 'India']
 
 
 class Data:
 
     def __init__(self, url, countries, fileName):
         self.url = url
-        self.countries = countries
+        self.countries = countries.copy()
         self.fileName = fileName
 
-    def processData(self):
+    def processData(self, totalPreFix, dailyPreFix):
 
         df = pd.read_csv(self.url).copy()
+
+        df.to_csv('raw_' + self.fileName)
+        print('Saved: raw_' + self.fileName)
 
         del df['Lat'], df['Long']
 
@@ -26,26 +35,36 @@ class Data:
 
         df.set_index("Date", inplace=True)
 
+        if len(self.countries) == 0:
+            print('list is empty')
+        else:
+            self.countries.remove('Date')
+
+        for x in self.countries:
+            df[totalPreFix + x] = df[x]
+            df[dailyPreFix + x] = df[x] - df[x].shift().fillna(0)
+            del df[x]
+
         df.to_csv(self.fileName)
 
+        print('Saved: ' + self.fileName)
 
-urlGlabalConfirmed = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
-urlGlabalRecovered = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
-urlGlabalDeaths = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+    def merge():
+        f1 = pd.read_csv('GlabalConfirmed.csv', index_col=[0])
+        f2 = pd.read_csv('GlabalDeath.csv', index_col=[0])
+        f3 = pd.read_csv('GlabalRecovered.csv', index_col=[0])
 
-countries = ['Date', 'US', 'Canada', 'France', 'India']
+        pd.concat([f1, f2, f3], axis=1).to_csv('Merged.csv')
+        print('Saved: Merged.csv')
+
 
 GlabalConfirmed = Data(urlGlabalConfirmed, countries, 'GlabalConfirmed.csv')
-GlabalConfirmed.processData()
+GlabalConfirmed.processData('Total_Confirmed_', 'Daily_Confirmed_')
 
 GlabalRecovered = Data(urlGlabalRecovered, countries, 'GlabalRecovered.csv')
-GlabalRecovered.processData()
+GlabalRecovered.processData('Total_Recovered_', 'Daily_Recovered_')
 
-GlabalDeaths = Data(urlGlabalDeaths, countries, 'GlabalDeaths.csv')
-GlabalDeaths.processData()
+GlabalDeaths = Data(urlGlabalDeaths, countries, 'GlabalDeath.csv')
+GlabalDeaths.processData('Total_Death_', 'Daily_Death_')
 
-# df = df.asfreq('b')
-
-# df.isna().sum()
-
-# df['United Kingdom'].diff().plot(figsize=(20,5), xlabel='date')
+Data.merge()
--- covid.py
+++ covid.py
@@ -2,39 +2,55 @@
 # In[ ]:
 
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-urlGlabalConfirmedCases = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
-
-urlGlabalRecoveredCases = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
-countries = ['date', 'US', 'Canada', 'France', 'India']
-
-df = pd.read_csv(urlGlabalConfirmedCases).copy()
-
-del df['Lat'], df['Long']
+from pandas import read_csv
+from Data import *
 
 
-df = df.rename(columns={
-    'Province/State': 'State',
-    'Country/Region': 'Country'
-}, inplace=False)
+# In[ ]:
 
 
-df = df.groupby(['Country'], as_index=True, group_keys=True).sum().T.rename_axis(
-    'date').reset_index()
-
-df = df[countries]
-
-df['date'] = pd.to_datetime(df['date'], utc=True)
-
-df.set_index('date', inplace=True)
-
-df.to_csv(r'confirmed_global.csv')
+startDate = '2021-01'
+endDate = '2021-05'
+cols_plot = ['Daily_Confirmed_US',
+             'Daily_Confirmed_France', 'Daily_Confirmed_India']
 
 
-# df = df.asfreq('b')
+urlGlabalConfirmed = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+urlGlabalRecovered = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
+urlGlabalDeaths = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
 
-# df.isna().sum()
+countries = ['Date', 'France', 'India']
 
-# df['United Kingdom'].diff().plot(figsize=(20,5), xlabel='date')
+
+GlabalConfirmed = Data(urlGlabalConfirmed, countries, 'GlabalConfirmed.csv')
+GlabalConfirmed.processData('Total_Confirmed_', 'Daily_Confirmed_')
+
+GlabalRecovered = Data(urlGlabalRecovered, countries, 'GlabalRecovered.csv')
+GlabalRecovered.processData('Total_Recovered_', 'Daily_Recovered_')
+
+GlabalDeaths = Data(urlGlabalDeaths, countries, 'GlabalDeath.csv')
+GlabalDeaths.processData('Total_Death_', 'Daily_Death_')
+
+Data.merge()
+
+# In[ ]:
+df = read_csv('Merged.csv', header=0,
+              parse_dates=[0], index_col=0)
+
+df.plot(
+    figsize=(20, 10),
+    linewidth=0.8,
+    marker='.',
+    xlabel='Date', ylabel='No. of cases',
+    y=cols_plot,
+    title='Glabal Daily Confirmed Cases'
+).grid(color='gray', linewidth=0.3)
+
+# df.loc[startDate:endDate].plot(
+#     figsize=(20, 10),
+#     linewidth=0.8,
+#     marker='.',
+#     xlabel='Date', ylabel='No. of cases',
+#     y=cols_plot,
+#     title='Glabal Daily Confirmed Cases'
+# ).grid(color='gray', linewidth=0.3)
